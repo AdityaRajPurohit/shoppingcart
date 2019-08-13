60360bc890e5ff44696c9d800639535644a0eeb0
Aditya Purohit <purohit.adiraj30@gmail.com>
2019-08-13 13:49:14 +0530

    commit

--- src/component/Bill.js
+++ src/component/Bill.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
-
+import './Item.css';
 export default class Bill extends Component {
     render() {
         return (
-            <div>
-                <div className="card col-3 table-responsive">
+            <div className="bill">
+                <div className="bill-container card col-3 table-responsive">
                     <table className="table">
                         <thead>
                             <th span="row">Price Details</th>
                             <tr>Total Price</tr>
+                            <tr>$13</tr>
+
                         </thead>
                     </table>
                 </div>
